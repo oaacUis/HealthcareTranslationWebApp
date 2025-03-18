380e023bd29e300fd834dec54c59624fb7177ece
Otto <otto2190403@correo.uis.edu.co>
2025-03-18 12:45:09 -0500

    Implement logging utility module

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
-from database import SessionLocal
+from ..database import SessionLocal
 from ..database import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
